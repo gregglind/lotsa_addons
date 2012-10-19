b7890bac2b3e7ed4753982acafe8bef1636b461c
Gregg Lind <gregg.lind@gmail.com>
2012-10-19 14:53:48 -0500

    lots of nit fixes

--- testy/fakey/lib/main.js
+++ testy/fakey/lib/main.js
@@ -1,32 +1,31 @@
 if (typeof __$coverObject === "undefined"){
-	var __$coverObject = {};
-	if (typeof window !== "undefined") window.__$coverObject = __$coverObject;
-	else if (typeof global !== "undefined") global.__$coverObject = __$coverObject;
+	if (typeof window !== "undefined") window.__$coverObject = {};
+	else if (typeof global !== "undefined") global.__$coverObject = {};
+	else throw new Error("cannot find the global scope");
 }
-console.log("coverobject is:", JSON.stringify(__$coverObject));
 __$coverObject["lib/main.js"] = {};
 __$coverObject["lib/main.js"].__code = "\nlet main = function(){\n    if (true) {\n        console.log(\"horray for consistency\")\n        return 1;\n    } else {\n        console.error(\"uh-oh, the axis has tilted\")\n    };\n};\n\nexports.main = main;\n";
-__$coverObject["lib/main.js"]["1:178"] = 0;
-__$coverObject["lib/main.js"]["180:200"] = 0;
-__$coverObject["lib/main.js"]["28:174"] = 0;
-__$coverObject["lib/main.js"]["174:175"] = 0;
-__$coverObject["lib/main.js"]["48:94"] = 0;
-__$coverObject["lib/main.js"]["94:103"] = 0;
-__$coverObject["lib/main.js"]["125:173"] = 0;
-__$coverObject['lib/main.js']['1:178']++;
+__$coverObject["lib/main.js"]["1:177"] = 0;
+__$coverObject["lib/main.js"]["180:199"] = 0;
+__$coverObject["lib/main.js"]["28:173"] = 0;
+__$coverObject["lib/main.js"]["174:174"] = 0;
+__$coverObject["lib/main.js"]["48:93"] = 0;
+__$coverObject["lib/main.js"]["94:102"] = 0;
+__$coverObject["lib/main.js"]["125:172"] = 0;
+__$coverObject['lib/main.js']['1:177']++;
 let main = function () {
-    __$coverObject['lib/main.js']['28:174']++;
+    __$coverObject['lib/main.js']['28:173']++;
     if (true) {
-        __$coverObject['lib/main.js']['48:94']++;
+        __$coverObject['lib/main.js']['48:93']++;
         console.log('horray for consistency');
-        __$coverObject['lib/main.js']['94:103']++;
+        __$coverObject['lib/main.js']['94:102']++;
         return 1;
     } else {
-        __$coverObject['lib/main.js']['125:173']++;
+        __$coverObject['lib/main.js']['125:172']++;
         console.error('uh-oh, the axis has tilted');
     }
-    __$coverObject['lib/main.js']['174:175']++;
+    __$coverObject['lib/main.js']['174:174']++;
     ;
 };
-__$coverObject['lib/main.js']['180:200']++;
+__$coverObject['lib/main.js']['180:199']++;
 exports.main = main;
