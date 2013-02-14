27cd939c2f1a3c4cdb76fda69705d76ccde97433
Gregg Lind <gregg.lind@gmail.com>
2013-02-14 13:45:16 -0600

    now with listener

--- change_urlbar_text/lib/main.js
+++ change_urlbar_text/lib/main.js
@@ -1,11 +1,15 @@
 
 "use strict";
 
+let recorder=require("micropilot").Micropilot("addressbar").start();
+
 let possibles = [
 	"where to do today?",
 	"search or enter an address",
 	"your wish is my command"
 ];
+
+require('simple-prefs').prefs.logtoconsole=true
 
 const BROWSER = "chrome://browser/content/browser.xul";
 const {WindowTracker} = require("sdk/deprecated/window-utils");
@@ -14,6 +18,10 @@ let addressbarTracker = new WindowTracker({
 	onTrack: function(window){
 		if (BROWSER != window.location) return;
 		let which = Math.floor(Math.random() * possibles.length)
-		window.gURLBar.placeholder = " "+possibles[which]
+		window.gURLBar.placeholder = possibles[which]
+		window.gURLBar.addEventListener('click',function(){
+			console.log('clicked!',possibles[which]);
+			recorder.record({ts:Date.now(),msg:'searchbar',txt:possibles[which]})
+		})
 	}
 });
