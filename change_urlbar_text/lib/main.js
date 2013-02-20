d7ce31278045039ac21939c35243ffb81b127632
Gregg Lind <gregg.lind@gmail.com>
2013-02-20 15:28:11 -0600

    simpler possibles logic

--- change_urlbar_text/lib/main.js
+++ change_urlbar_text/lib/main.js
@@ -9,6 +9,12 @@ let possibles = [
 	"your wish is my command"
 ];
 
+let random_possible = function(){
+	let which = Math.floor(Math.random() * possibles.length)
+	let t = possibles[which];
+	return t
+}
+
 require('simple-prefs').prefs.logtoconsole=true
 
 const BROWSER = "chrome://browser/content/browser.xul";
@@ -17,8 +23,8 @@ const {WindowTracker} = require("sdk/deprecated/window-utils");
 let addressbarTracker = new WindowTracker({
 	onTrack: function(window){
 		if (BROWSER != window.location) return;
-		let which = Math.floor(Math.random() * possibles.length)
-		window.gURLBar.placeholder = possibles[which]
+		let t = random_possible()
+		window.gURLBar.placeholder = t;
 		window.gURLBar.addEventListener('click',function(){
 			console.log('clicked!',possibles[which]);
 			recorder.record({ts:Date.now(),msg:'searchbar',txt:possibles[which]})
