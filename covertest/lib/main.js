272258205e964e26c1ce8a7b3635a0c68544f220
Gregg Lind <gregg.lind@gmail.com>
2013-04-28 14:51:07 -0500

    broken globals re https://bugzilla.mozilla.org/show_bug.cgi?id=866535

---
+++ covertest/lib/main.js
@@ -0,0 +1,16 @@
+if (typeof __$coverObject === "undefined"){
+    var __$coverObject;
+    if (typeof window !== "undefined"){
+        if (window.__$coverObject === undefined) {window.__$coverObject = {} }
+        __$coverObject = window.__$coverObject;
+    } else if (typeof global !== "undefined") {
+        if (global.__$coverObject === undefined) {global.__$coverObject = {} }
+        __$coverObject = global.__$coverObject;
+    } else {
+      throw new Error("cannot find the global scope");
+
+    }
+}
+
+true;
+
